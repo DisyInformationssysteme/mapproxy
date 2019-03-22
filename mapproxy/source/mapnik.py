dce3bfe6ed2b03d4f8bf87bb804860122e24fd13
Arne Babenhauserheide <arne_bab@web.de>
2019-03-22 12:11:48 +0100

    use concurrent_tile_creators

--- mapproxy/source/mapnik.py
+++ mapproxy/source/mapnik.py
@@ -52,7 +52,7 @@ class MapnikSource(MapLayer):
     supports_meta_tiles = True
     def __init__(self, mapfile, layers=None, image_opts=None, coverage=None,
                  res_range=None, lock=None, reuse_map_objects=False, scale_factor=None,
-                 concurrent_requests=None):
+                 concurrent_tile_creators=None):
         MapLayer.__init__(self, image_opts=image_opts)
         self.mapfile = mapfile
         self.coverage = coverage
@@ -75,7 +75,7 @@ class MapnikSource(MapLayer):
         _last_mapfile = None
         # pre-create more maps than the typical number of threads to allow for fast start
         global _map_objs_precreated
-        _precreate_count = (concurrent_requests if concurrent_requests else 3)
+        _precreate_count = (concurrent_tile_creators if concurrent_tile_creators else 3)
         print("XXX precreate count:", _precreate_count)
         _map_objs_precreated = queue.Queue(_precreate_count)
         self.map_obj_pre_creating_thread = threading.Thread(target=self._precreate_maps)
