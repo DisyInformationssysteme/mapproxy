9800270ab056422e6044be39bb2a9f467347213b
Arne Babenhauserheide <arne_bab@web.de>
2019-03-22 12:13:01 +0100

    debug

--- mapproxy/source/mapnik.py
+++ mapproxy/source/mapnik.py
@@ -75,8 +75,9 @@ class MapnikSource(MapLayer):
         _last_mapfile = None
         # pre-create more maps than the typical number of threads to allow for fast start
         global _map_objs_precreated
-        _precreate_count = (concurrent_tile_creators if concurrent_tile_creators else 3)
-        print("XXX precreate count:", _precreate_count)
+        
+        _precreate_count = (concurrent_tile_creators if concurrent_tile_creators else 4)
+        print("XXX precreate count:", _precreate_count, concurrent_tile_creators)
         _map_objs_precreated = queue.Queue(_precreate_count)
         self.map_obj_pre_creating_thread = threading.Thread(target=self._precreate_maps)
         self.map_obj_pre_creating_thread.daemon=True
